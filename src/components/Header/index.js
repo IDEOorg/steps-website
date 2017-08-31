12bc1eba00d15cf43d89ffa8e178f59b57085b93
Michael Zhong <mzhong@org-m-mzhong.ideo.com>
2017-08-31 18:52:01 -0400

    partially done styling header bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,40 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 import Button from '../Button';
 import './index.less';
 import config from '../../data/config';
 import logo from '../../assets/steps-logo.svg';
+import menuIcon from '../../assets/menu-icon.svg';
+import exitIcon from '../../assets/menu-close.svg';
 
-const Header = (props) => {
-  const links = config.headerbar.map((content) => {
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isExpanded: false};
+  }
+  render() {
+    const links = config.headerbar.map((content) => {
+      return (
+        <a className="header_link" href={`#${content.url}`} key={content.url}>{content.name}</a>
+      );
+    });
+    let guideButton = null;
+    if (this.props.showGuideButton) {
+      guideButton = (
+        <Button styling="header_button" url="https://steps.ideo.org/landing-page">
+          <p>TRY A GUIDE</p>
+        </Button>
+      );
+    }
+
+    let iconShown = null;
+    if (this.state.isExpanded) {
+      iconShown = (
+        <img src={exitIcon} alt="Exit Icon" />
+      );
+    } else {
+      iconShown = (
+        <img src={menuIcon} alt="Menu Icon" />
+      );
+    }
+
     return (
-      <a className="header_link" href={`#${content.url}`} key={content.url}>{content.name}</a>
-    );
-  });
-  let guideButton = null;
-  if (props.showGuideButton) {
-    guideButton = (
-      <Button styling="header_button" url="https://steps.ideo.org/landing-page">
-        <p>TRY A GUIDE</p>
-      </Button>
+      <div className={classNames("header", this.props.styling)}>
+        <div className="logo_section">
+          <a href="#/">
+            <img src={logo} alt="Steps Logo" />
+          </a>
+        </div>
+        <div className="links_section">
+          <div
+            className="menu_icon"
+            onClick={() => { this.setState({ isExpanded: !this.state.isExpanded }); }}
+          >
+            {iconShown}
+          </div>
+          <div className="links_content">
+            {links}
+            {guideButton}
+          </div>
+        </div>
+      </div>
     );
   }
-
-  return (
-    <div className={classNames("header", props.styling)}>
-      <div className="logo_section">
-        <a href="#/">
-          <img src={logo} alt="Steps Logo" />
-        </a>
-      </div>
-      <div className="links_section">
-        {links}
-        {guideButton}
-      </div>
-    </div>
-  );
-};
-
-export default Header;
+}
 
 Header.displayName = 'Header';
