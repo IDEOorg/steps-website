d8feab3f892c8d3e25c16210463eb8857e835aa7
Michael Zhong <mzhong@org-m-mzhong.ideo.com>
2017-11-15 18:25:13 -0500

    footer style changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,12 +5,24 @@ const logo = require('../../assets/ideoorg-logo.svg');
 
 const Footer = () => {
   return (
-    <div className="about_ideo_section">
-      <div className="about_ideo_logo_box">
-        <p className="about_ideo_logo_text">A PROJECT BY</p>
-        <a href="http://www.ideo.org">
-          <img className="about_ideo_logo" src={logo} alt="Logo" />
-        </a>
+    <div>
+      <div className="about_ideo_section">
+        <div className="about_ideo_logo_box">
+          <p className="about_ideo_logo_text">A PROJECT BY</p>
+          <a href="http://www.ideo.org">
+            <img className="about_ideo_logo" src={logo} alt="Logo" />
+          </a>
+        </div>
+      </div>
+      <div className="lp-footer-section">
+        <div className="lp-footer-copyright">
+          <p>© 2017 IDEO.org</p>
+        </div>
+        <div className="lp-footer-info">
+          <a href="https://www.ideo.org/privacy-policy">Privacy Policy</a>
+          <a href="https://www.ideo.org/terms-of-use">Terms of Use</a>
+          <a href="http://steps.ideo.org/#/mission">Contact Us</a>
+        </div>
       </div>
     </div>
   );
